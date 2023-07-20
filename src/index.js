4f992ceddcbc570ad5767f928c227b69ad0e542a
B2014774 <phub2014774@student.ctu.edu.vn>
2023-07-20 15:24:15 +0700

    20/7

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const hbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-// HTTP Logger
-app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+// // HTTP Logger
+// app.use(morgan('combined'))
 
 // Template Engine
 app.engine('hbs', hbs.engine({
@@ -23,6 +25,17 @@ app.get('/', (req, res) => {
 // Render news
 app.get('/news', (req, res) => {
   res.render('news');
+})
+
+//render Search
+app.get('/search',(req, res) => {
+  console.log(req.query.ref);
+  res.render('search');
+})
+
+//testing
+app.get('/noob', (req, res) => {
+  res.send('<h1>A new Buffalo!</h1>');
 })
 
 
